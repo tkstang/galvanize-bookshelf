46a19a4c99eb22e8e02732db2b6164fc9064e2a7
Thomas Stang <stang.tk@gmail.com>
2017-03-10 15:41:34 -0800

    added setval for users seed file

--- seeds/01users.js
+++ seeds/01users.js
@@ -16,4 +16,7 @@ exports.seed = function(knex, Promise) {
 				}),
       ]);
     });
+		.then(function(){
+			return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
+		});
 };
